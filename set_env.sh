63ad73e8241b81abc5a5d2fe1f3f594ce58d2e42
wjclemens69 <62310455+wjclemens69@users.noreply.github.com>
2020-12-15 15:44:00 -0500

    Add files via upload
    Initial Commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=wjcemens69
+export POSTGRES_PASSWORD=SusanitiEmily08!
+export POSTGRES_HOST=clemensdbilesson4.cnjtgqm0k1qq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=clemensdblesson4
+export AWS_BUCKET=l4project-758290640424-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=
+export URL=http://localhost:8100
+export aws_media_bucket=l4project-758290640424-dev
+export aws_region=us-east-1
